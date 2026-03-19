480064bf7479a9468b6a893510709cf33690ed59
Eric Davila <eric.davila@eseisa.com>
2026-03-19 20:38:32 +0000

    grafica 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🎈 Esta es mi nueva aplicación")
 st.write(
-    "Empecemos la construcción!."
+    "Hola mundo!."
 )
+
+ 
+df = pd.DataFrame({'dia': [1, 2, 3, 4], 'demanda': [20, 30, 15, 35]})
+st.line_chart(df)
+
+
