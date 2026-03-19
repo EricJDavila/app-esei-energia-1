9e8e7835281976e4c15af8dd86988ea11699cbf7
Eric Davila <eric.davila@eseisa.com>
2026-03-19 21:10:25 +0000

    dispersión

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,21 @@
-import streamlit as st
+import altair as alt
 import pandas as pd
+import streamlit as st
+from numpy.random import default_rng as rng
 
-st.title("🎈 Esta es mi nueva aplicación")
-st.write(
-    "Hola mundo!."
+st.title("Título de la aplicación")
+
+st.markdown("**Una aplicación**")
+
+
+df = pd.DataFrame(rng(0).standard_normal((200, 3)), columns=["a", "b", "c"])
+chart = (
+    alt.Chart(df)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
 )
 
- 
-df = pd.DataFrame({'demanda 1': [10, 26, 13, 28], 'demanda 2': [20, 30, 15, 35]})
-st.line_chart(df)
+st.write(chart)
 
-
+# df = pd.DataFrame({'demanda 1': [10, 26, 13, 28], 'demanda 2': [20, 30, 15, 35]})
+# st.line_chart(df)
